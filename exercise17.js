06a71cb530e540aa2ec191300f14963e005c6efa
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 13:24:04 +0200

    Add files via upload

---
+++ exercise17.js
@@ -0,0 +1,11 @@
+let age = 19;
+let conditionA = age >= 18;
+let str=""
+if(conditionA){
+  str="you can drive a car";
+  console.log(str);
+} 
+else{
+  str="you are too young to drive";
+  console.log(str);
+}
