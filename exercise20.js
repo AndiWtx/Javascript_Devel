72a3b7c8024c81f35c893b8237b4cb8bd272cfad
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 14:25:11 +0200

    Add files via upload

---
+++ exercise20.js
@@ -0,0 +1,11 @@
+let primitive= 56;
+
+if(typeof(primitive)== "number"){
+  console.log("It's a number")
+}else if(typeof(primitive)== "string"){
+  console.log("It's a string")
+}else if(typeof(primitive)== "boolean"){
+  console.log("It's a boolean")
+}else {
+  console.log("I don't know what it is")
+}
