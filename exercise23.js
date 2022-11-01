8c57b63db18465477afa1450f448ecf9233098e4
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 17:15:27 +0200

    Add files via upload

---
+++ exercise23.js
@@ -0,0 +1,13 @@
+let typeOfRoom= "junior suite room";
+
+switch(typeOfRoom){
+  case ("basic room") :
+    console.log("Price for " +typeOfRoom+ " --> 50e");
+    break;
+  case "junior suite room" :
+    console.log("Price for " +typeOfRoom+ " --> 80e" );
+    break;
+  case "master suite" :
+    console.log("Price for " +typeOfRoom+ " --> 100e" );
+    break;
+}
