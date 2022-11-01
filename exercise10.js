e970410def1ae47d692b86e16116da48e9bc652a
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 12:08:32 +0200

    Add files via upload

---
+++ exercise10.js
@@ -0,0 +1,8 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted
+yearsCompleted === firstYearCompleted
+
+// double equals is used for equality, js is converting the values to a same value
+// triple equals is also used for equality, but the type of variables remains the same
