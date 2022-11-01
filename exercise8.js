ec4d08736593c4550f07467484c80edba0625764
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 11:47:24 +0200

    Add files via upload

---
+++ exercise8.js
@@ -0,0 +1,20 @@
+let variable= 'true';
+
+console.log(typeof variable);
+console.log(variable);
+
+variable= Number(true);
+
+console.log(typeof variable);
+console.log(variable);
+
+variable=String("true");
+
+console.log(typeof variable);
+console.log(variable);
+
+variable= Boolean(true);
+
+console.log(typeof variable);
+console.log(variable);
+
