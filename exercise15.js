2cf1231f5a60d3917a99565b474e1b5a28c4cd9a
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-01 13:07:37 +0200

    Update exercise15.js

--- exercise15.js
+++ exercise15.js
@@ -1,6 +1,6 @@
 let age = 16;
 let conditionA = age >= 18;
 
-let drivable= conditionA ? "you can drive a car" : "you can't drive a car";
+let drivable= conditionA ? "you can drive a car" : "you are too young to drive";
 
 console.log(drivable);
