6446023af5b24c99b5c816ec50d431410c1380ea
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2022-11-27 00:07:31 +0200

    Add files via upload
    Exercise done with Mihai Mitricel

---
+++ es24.js
@@ -0,0 +1,10 @@
+function Smartphone(brand,name,price){
+    this.brand = brand;
+    this.name = name;
+    this. price = price;
+}
+let iphone = new Smartphone ("Apple", "13 Pro Max", 5250);
+console.log(iphone);
+let samsung = new Smartphone ("Samsung","S22", 3049)
+console.log(samsung);
+// this was done as a group project between me and Mihai Mitricel
